91b167f875a1fbc8dc99b279540df9b686cfe2ec
seorinn <seorin@ajou.ac.kr>
2024-01-29 00:25:30 +0900

    feat: 타임캠슐 오픈 페이지

---
+++ src/components/Menu.js
@@ -0,0 +1,69 @@
+import "../style/Menu.css";
+import { useState } from "react";
+
+import icon_close from "../assets/icon_close.png";
+import icon_profile from "../assets/icon_profile.png";
+import icon_setting from "../assets/icon_setting.png";
+import icon_capsule from "../assets/icon_capsule.png";
+import icon_chat from "../assets/icon_chat.png";
+import icon_instagram from "../assets/icon_instagram.png";
+import boundary_line from "../assets/boundary_line.png";
+
+const Menu = ({menuHandler}) => {
+    const userName = "린서";
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isSocial, setIsSocial] = useState(true);
+
+    const loginHandler = () => {
+        setIsLoggedIn(!isLoggedIn);
+    }
+    const closeMenu = () => {
+        menuHandler();
+    }
+
+    return(
+        <div className="Menu">
+            <img className="btn_close" alt="" src={icon_close} onClick={closeMenu}/>
+            <div className="buttons">
+            {isLoggedIn ? 
+                <div>
+                <div className="button_section">
+                    <img className="profile" alt="" src={icon_profile}/>
+                    <div className="profile">
+                        <button>{userName} 님</button>
+                        {isSocial && <p>카카오톡으로 로그인 중</p>}
+                    </div>
+                </div>
+                <div className="button_section">
+                    <img alt="" src={icon_setting}/>
+                    <button>계정 설정</button>
+                </div>
+            </div>
+            : <div>
+                <div className="button_section">
+                    <img className="profile" alt="" src={icon_profile}/>
+                    <div className="profile">
+                        <button onClick={loginHandler}>로그인</button>
+                        <p>현재 비로그인 상태입니다.</p>
+                    </div>
+                </div>
+            </div>}
+            <div className="button_section">
+                <img alt="" src={icon_capsule}/>
+                <button>타임캡슐 조회</button>
+            </div>
+            <img className="line" alt="" src={boundary_line}/>
+            <div className="button_section">
+                <img alt="" src={icon_chat}/>
+                <button>질문/문의</button>
+            </div>
+            <div className="button_section">
+                <img alt="" src={icon_instagram}/>
+                <button>인스타그램</button>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
