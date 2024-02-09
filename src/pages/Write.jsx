8977fa97fb5e4fd35e8a36f10b6aa5865199a2b6
seorinn <80616701+seorinn@users.noreply.github.com>
2024-02-09 18:08:23 +0900

    refactor: useLocation 추가 및 데이터 refactoring, post 임의 작성

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -2,29 +2,37 @@
 
 import "../styles/Write.css";
 
-import React, { useRef } from "react";
-import { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState  } from "react";
+import { useNavigate } from "react-router";
+import { useLocation } from "react-router-dom";
 
 import icon_alignCenter from "../assets/icon_alignCenter.png";
 import icon_alignLeft from "../assets/icon_alignLeft.png";
 import icon_alignRight from "../assets/icon_alignRight.png";
 import icon_camera from "../assets/icon_camera.png";
-import { useNavigate } from "react-router";
+import axios from "axios";
 
-//import axios
 const Write = () => {
   const navigate = useNavigate();
   const fileInputRef = useRef(null);
-  const [data, setData] = useState({
-    // recipient: "",
-    // image: "",
-    // text: "",
-    alignType: "left",
-    theme: "default",
-  });
-  const [items, setItems] = useState([{ type: 'text', content: '' }]);
+  // const location = useLocation();
+  // const receivedState = location.state;
+  // console.log(state);
 
-  // useEffect(() => {}, []);
+  //임시
+  const receivedState = {
+    pcapsule_name: 'pcapsule_name',
+    open_date: 'open_date',
+    dear_name: 'dear_name',
+    theme: 'theme',
+    content_type: 'format',
+  }
+
+  const [state, setState] = useState({
+    ...receivedState,
+    contents: [{ type: 'text', content: '' }],
+    alignType: "left",
+  });
 
   //임시저장
   const handleSave = () => {
@@ -32,18 +40,19 @@ const Write = () => {
   };
 
   //타임캡슐 미리보기
-  const goOverview = () => {
-    navigate(`/`);
-  };
+  // const goOverview = () => {
+  //   navigate(`/`);
+  // };
 
-  const handleSubmit = () => {
+  const handleSubmit = async() => {
     if (window.confirm("작성을 끝낼까요?")) {
+      console.log(state);
       // try{
-      //     axios.post(SERVER_URL, data);
+      //     await axios.post('SERVER_URL', state);
+          // navigate(`/`);
       // }catch(err){
-      //     window.alert(err);
+      //     alert(err.response.data.message);
       // }
-      // navigate(`/`);
     }
   };
 
@@ -63,7 +72,7 @@ const Write = () => {
 
   //이미지 첨부/삭제
   const addItem = () => {
-    const imageCount = items.filter(item => item.type === 'image').length;
+    const imageCount = state.contents.filter(item => item.type === 'image').length;
     if(imageCount < 3){
       fileInputRef.current.click();
     } else{
@@ -75,16 +84,24 @@ const Write = () => {
     //선택한 파일에 대한 작업 수행
     const selectedFile = e.target.files[0];
     if (selectedFile) {
+      // 이미지 연달아 첨부하는 경우
+      if(state.contents[state.contents.length-1].content.length === 0){ 
+        state.contents.splice(state.contents.length-1, 1);
+      }
       //선택한 파일 읽기
       const reader = new FileReader();
       reader.onload = (event) => {
-        setItems([...items, { type: 'image', content: event.target.result }, { type: 'text', content: '' }]);
+        setState({
+          ...state,
+          contents: [...state.contents, { type: 'image', content: event.target.result }, { type: 'text', content: '' }]
+        });
       };
       reader.readAsDataURL(selectedFile);
     }
   };
+
   const handleContentChange = (index, content) => {
-    const newItems = [...items];
+    const newItems = [...state.contents];
     newItems[index].content = content;
 
     const allText = newItems.reduce((acc, item) => {
@@ -95,60 +112,63 @@ const Write = () => {
     }, '');
 
     if(allText.length <= 1000){
-      setItems(newItems);
+      setState({
+        ...state,
+        contents: newItems,
+      })
     } else{
       alert("텍스트 내용은 1000자를 초과할 수 없습니다.");
     }
   };
   const deleteImage = async(index) => {
     if (window.confirm("이미지를 삭제할까요?")) {  
-      const newItems = [...items];
+      const newItems = [state.contents];
       newItems[index - 1].content += '\n' + newItems[index + 1].content;
       newItems.splice(index, 2);
-      await setItems(newItems);
+      await setState({
+        ...state,
+        contents: newItems,
+      })
       await autoResizeTextarea(index);
     }
   };
 
   //텍스트 좌, 우, 가운데 정렬
   const changeAlignType = () => {
-    if (data.alignType === "left") {
-      setData({
-        ...data,
+    if (state.alignType === "left") {
+      setState({
+        ...state,
         alignType: "center",
       });
-    } else if (data.alignType === "center") {
-      setData({
-        ...data,
+    } else if (state.alignType === "center") {
+      setState({
+        ...state,
         alignType: "right",
       });
     } else {
-      setData({
-        ...data,
+      setState({
+        ...state,
         alignType: "left",
       });
     }
   };
-
   return (
-    <div className={["Write", data.theme].join(" ")}>
+    <div className={["Write", state.theme].join(" ")}>
       <div className="write_top">
         <button className="btn_save" onClick={handleSave}>임시저장</button>
       </div>
       <div className="write_center">
         <div className="to">
-          <p>To. ME</p>
-          {/* <input></input> */}
+          <p>To. {state.dear_name}</p>
         </div>
-
         <div className="text">
-          {items.map((item, index) => (
+          {state.contents.map((item, index) => (
           <div key={index}>
             {item.type === 'text' ? (
               <textarea
                 value={item.content}
                 onChange={(e) => handleContentChange(index, e.target.value)}
-                className={`${data.alignType} index${index}`}
+                className={`${state.alignType} index${index}`}
                 placeholder="여기에 작성하세요."
                 onKeyDown={autoResizeTextarea}
                 onKeyUp={autoResizeTextarea}
@@ -179,7 +199,7 @@ const Write = () => {
             ref={fileInputRef}
             onChange={handleFileChange}
           />
-          {data.alignType === "left" && (
+          {state.alignType === "left" && (
             <img
               className="btn_align"
               onClick={changeAlignType}
@@ -187,7 +207,7 @@ const Write = () => {
               src={icon_alignLeft}
             />
           )}
-          {data.alignType === "center" && (
+          {state.alignType === "center" && (
             <img
               className="btn_align"
               onClick={changeAlignType}
@@ -195,7 +215,7 @@ const Write = () => {
               src={icon_alignCenter}
             />
           )}
-          {data.alignType === "right" && (
+          {state.alignType === "right" && (
             <img
               className="btn_align"
               onClick={changeAlignType}
