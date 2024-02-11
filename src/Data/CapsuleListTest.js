653c36c3aa8511991f63389269aa87afb50f890c
hienjoy <wisdom16no@naver.com>
2024-02-11 23:51:02 +0900

    test : 홈 화면 캡슐 목록 테스트 데이터

---
+++ src/Data/CapsuleListTest.js
@@ -0,0 +1,34 @@
+//홈화면 팝업 캡슐 리스트 테스트 데이터
+
+const capsuleList = [
+    {
+        capsule_name: '타임캡슐 1',
+        capsule_number: 'WIT_00001',
+    },
+    {
+        capsule_name: '타임캡슐 2',
+        capsule_number: 'WIT_00001',
+    },
+    {
+        capsule_name: '타임캡슐 3',
+        capsule_number: 'WIT_00001',
+    },
+    {
+        capsule_name: '타임캡슐 4',
+        capsule_number: 'WIT_00001',
+    },
+    {
+        capsule_name: '타임캡슐 5',
+        capsule_number: 'WIT_00001',
+    },
+    {
+        capsule_name: '타임캡슐 6',
+        capsule_number: 'WIT_00001',
+    },
+    {
+        capsule_name: '타임캡슐 7',
+        capsule_number: 'WIT_00001',
+    },
+  ];
+  
+  export default capsuleList;
