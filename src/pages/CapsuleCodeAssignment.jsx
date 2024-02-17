eaf234e0ee81855fc0c7899f964bd7cdccd9b879
seorinn <seorin@ajou.ac.kr>
2024-02-17 21:22:38 +0900

    design: 마우스 포인터 형태, 이미지 user-select 등 통일화

--- src/pages/CapsuleCodeAssignment.jsx
+++ src/pages/CapsuleCodeAssignment.jsx
@@ -83,54 +83,56 @@ const CapsuleCodeAssignment = ({ initialNickname }) => {
   };
 
   return (
-    <div className="capsule-code-assignment">
-      <p className="message">캡슐번호를 기억하세요!</p>
-      <div className="code-box">
-        <div className="code-container">
-          <span className="code">캡슐번호&nbsp;</span>
-          <span className="assigned-code">
-            |&nbsp;{location.state.capsule_number}
-          </span>
+    <div className="CapsuleCodeAssignment">
+      <div className="container">
+        <p className="message">캡슐번호를 기억하세요!</p>
+        <div className="code-box">
+          <div className="code-container">
+            <span className="code">캡슐번호&nbsp;</span>
+            <span className="assigned-code">
+              |&nbsp;{location.state.capsule_number}
+            </span>
+          </div>
+          <div className="copy-container">
+            <img src={Copyimg} alt="복사하기" className="copy-img" />
+            <CopyToClipboard
+              text={location.state.capsule_number}
+              onCopy={handleCopyClick}
+            >
+              <span className="copy">복사</span>
+            </CopyToClipboard>
+          </div>
         </div>
-        <div className="copy-container">
-          <img src={Copyimg} alt="복사하기" className="copy-img" />
-          <CopyToClipboard
-            text={location.state.capsule_number}
-            onCopy={handleCopyClick}
-          >
-            <span className="copy">복사</span>
-          </CopyToClipboard>
+        <div className="password-box">
+          <span className="password">비밀번호&nbsp;&nbsp;</span>
+          <span className="divide">|&nbsp;&nbsp;</span>
+          <input
+            type="password"
+            maxLength="6"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="숫자 6자"
+            className="password-input"
+          ></input>
         </div>
+        <div className="guide-box">
+          <p className="guide-title">&nbsp;&nbsp;꼭 읽어보세요!</p>
+          <p className="guide-content">
+            • 캡슐번호는 홈화면에서 “타임캡슐 확인하기"를 통해 다시 확
+            <br />
+            &nbsp;&nbsp;&nbsp;인하실 수 있습니다.
+          </p>
+          <p className="guide-content">
+            • 비밀번호는 설정 후 변경할 수 없으며, 캡슐을 오픈할 때 캡슐
+            <br />
+            &nbsp;&nbsp;&nbsp;번호와 함께 쓰이기 때문에 꼭 기억해주세요.
+          </p>
+        </div>
+        <button className="btn" onClick={handleSavePassword}>
+          확인했어요!
+        </button>
+        {copied && <div className="copied-message">복사되었습니다!</div>}
       </div>
-      <div className="password-box">
-        <span className="password">비밀번호&nbsp;&nbsp;</span>
-        <span className="divide">|&nbsp;&nbsp;</span>
-        <input
-          type="password"
-          maxLength="6"
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder="숫자 6자"
-          className="password-input"
-        ></input>
-      </div>
-      <div className="guide-box">
-        <p className="guide-title">&nbsp;&nbsp;꼭 읽어보세요!</p>
-        <p className="guide-content">
-          • 캡슐번호는 홈화면에서 “타임캡슐 확인하기"를 통해 다시 확
-          <br />
-          &nbsp;&nbsp;&nbsp;인하실 수 있습니다.
-        </p>
-        <p className="guide-content">
-          • 비밀번호는 설정 후 변경할 수 없으며, 캡슐을 오픈할 때 캡슐
-          <br />
-          &nbsp;&nbsp;&nbsp;번호와 함께 쓰이기 때문에 꼭 기억해주세요.
-        </p>
-      </div>
-      <button className="btn" onClick={handleSavePassword}>
-        확인했어요!
-      </button>
-      {copied && <div className="copied-message">복사되었습니다!</div>}
     </div>
   );
 };
