a18729cb4369d0b6b18f9fd0215e4f65dad6fba6
김희은 <heeeun000711@gmail.com>
2024-02-18 14:23:29 +0900

    fix: 일반캡슐 userId 수정

--- src/pages/WritingFormat.jsx
+++ src/pages/WritingFormat.jsx
@@ -28,8 +28,7 @@ export default function WritingFormat() {
     }
     axios
       .post(`${process.env.REACT_APP_API_BASE_URL}/pcapsule/create`, {
-        userId: 1,
-        // userId: sessionStorage.getItem("userId"),
+        userId: sessionStorage.getItem("userId"),
         pcapsule_name: location.state.pcapsule_name,
         open_date: location.state.open_date,
         dear_name: location.state.dear_name,
