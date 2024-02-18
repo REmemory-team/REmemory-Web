4b0a07bd6e76252631a9e806ad39927dfb85f3b1
hienjoy <wisdom16no@naver.com>
2024-02-18 23:46:16 +0900

    fix: alert창, NULL 오류

--- src/pages/Nickname.jsx
+++ src/pages/Nickname.jsx
@@ -1,45 +1,38 @@
 // 카카오 로그인 - 이름(닉네임) 설정
 
 import "../styles/Nickname.css";
-
-import React, { useState, useEffect } from "react";
-
-import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default function Nickname() {
-  const [userNickname, setUserNickname] = useState("");
-
+  const [userNickname, setUserNickname] = useState(
+    sessionStorage.getItem("nickname") || ""
+  );
   const maxLength = 10;
   const navigate = useNavigate();
 
   //로그인 과정에서 받은 데이터(아이디)
   const userId = sessionStorage.getItem("userId");
 
-  useEffect(() => {
-    // 세션 스토리지에서 닉네임을 가져와서 설정합니다.
-    const storedNickname = sessionStorage.getItem("nickname");
-    // 세션 스토리지에서 가져온 값이 null이거나 undefined인 경우 빈 문자열("")로 설정합니다.
-    setUserNickname(storedNickname || "");
-  }, []);
-
+  //사용자가 입력한 닉네임 저장 함수
   const handleInputChange = (e) => {
-    setUserNickname(e.target.value);
+    if(e.target.value.length > maxLength){
+      alert("닉네임은 10자 이내로 설정해 주세요");
+    }
+    else setUserNickname(e.target.value);
   };
 
   //닉네임 유효성 검사
-  const validNickname = (nickname) => {
-    const regex =
-      /^(?!\s)([ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9?!@#$%^&*()-_+=~`'"\s]){1,10} $/;
+  const validNickname = (nickname) =>{
+    const regex = /^(?!\s)([ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9?!@#$%^&*()-_+=~`'"\s]){1,10}$/;
     return regex.test(nickname);
   };
 
   //이걸로 할게요! 버튼 누를시
   const handleSubmit = () => {
-    if (validNickname(userNickname)) {
-      // Bearer 토큰 가져오기
+    if(validNickname(userNickname)){
       const token = sessionStorage.getItem("token");
-
       axios
         .patch(
           `${process.env.REACT_APP_API_BASE_URL}/user/nickname`,
@@ -53,17 +46,17 @@ export default function Nickname() {
             },
           }
         )
-        .then((response) => {
-          console.log("닉네임설정-서버응답: ", response);
+        .then((response)=>{
+          console.log("서버응답: ",response);
           navigate("/login/kakao/home");
         })
-        .catch((error) => {
-          console.log("오류: ", error);
-        });
-    } else {
-      alert(
-        "한글, 영어, 숫자, 특수문자로 구성된 1~10자리 닉네임을 입력해주세요"
-      );
+        .catch((error)=>{
+          console.log("오류: ",error);
+        })
+        // navigate("/login/kakao/home", { state: { userNickname } });
+    }
+    else{
+      alert("한글, 영어, 숫자, 특수문자로 구성된 1~10자리 닉네임을 입력해주세요");
     }
   };
 
@@ -81,14 +74,7 @@ export default function Nickname() {
           value={userNickname}
           onChange={handleInputChange}
           maxLength={maxLength}
-          // 입력 제한을 위한 추가 설정
-          onInput={(e) => {
-            if (e.target.value.length > maxLength) {
-              e.target.value = e.target.value.slice(0, maxLength);
-            }
-          }}
         />
-
         <span>
           {userNickname.length}/{maxLength}
         </span>
