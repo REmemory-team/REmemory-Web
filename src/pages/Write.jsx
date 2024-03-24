72706986df015d6970e0781d58e409753b79bfa5
seorinn <80616701+seorinn@users.noreply.github.com>
2024-03-24 17:16:01 +0900

    feat: write/text에서 텍스트 사이 이미지 첨부 기능 추가

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -13,6 +13,7 @@ import icon_alignCenter_white from "../assets/icon_alignCenter_white.png";
 import icon_alignLeft_white from "../assets/icon_alignLeft_white.png";
 import icon_alignRight_white from "../assets/icon_alignRight_white.png";
 import icon_camera_white from "../assets/icon_camera_white.png";
+
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router";
 
@@ -33,6 +34,11 @@ const Write = () => {
     contents: [{ type: "text", content: "" }],
     alignType: "left",
   });
+  
+  const [cursor, setCursor] = useState({
+    pos: null,
+    index: null,
+  });
 
   useEffect(()=>{
     setReceivedState(location.state);
@@ -41,11 +47,10 @@ const Write = () => {
       capsule_number: location.state.capsule_number,
     })
   }, [location]);
-
-  //임시저장
-  const handleSave = () => {
-    window.alert("임시저장 되었습니다.");
-  };
+  
+  useEffect(()=>{
+    resizeTextarea(state.contents);
+  }, [state]);
 
   const handleSubmit = async () => {
     if (window.confirm("작성을 끝낼까요?")) {
@@ -70,21 +75,32 @@ const Write = () => {
   };
 
   //textarea 길이 조정
-  const autoResizeTextarea = (e) => {
-    let textarea;
-    {
-      typeof e === "number"
-        ? (textarea = document.querySelector(`.index${e - 1}`))
-        : (textarea = document.querySelector(`.index${e.target.id}`));
-    }
+  const autoResizeTextarea = (e, index) => {
+    setCursor({pos: e.target.selectionStart, index: index});
+    let textarea = document.querySelector(`.index${e.target.id}`);
     if (textarea) {
       textarea.style.height = "0";
       let height = textarea.scrollHeight;
       textarea.style.height = `${height}px`;
     }
   };
+  const resizeTextarea = (contents) => {
+    for(let i=0; i<contents.length; i++){
+      if(state.contents[i].type === "text"){
+        if(document.getElementById(`${i}`)){
+          document.getElementById(`${i}`).value = contents[i].content;        
+          let textarea = document.querySelector(`.index${i}`);
+          if (textarea) {
+            textarea.style.height = "0";
+            let height = textarea.scrollHeight;
+            textarea.style.height = `${height}px`;
+          }
+        }
+      }
+    }
+  }
 
-  //이미지 첨부/삭제
+  //이미지 첨부
   const addItem = () => {
     const imageCount = state.contents.filter(
       (item) => item.type === "image"
@@ -95,31 +111,77 @@ const Write = () => {
       alert("이미지는 최대 3장까지 추가할 수 있습니다.");
     }
   };
-
   const handleFileChange = (e) => {
-    //선택한 파일에 대한 작업 수행
     const selectedFile = e.target.files[0];
+    const newArray = state.contents;
     if (selectedFile) {
-      // 이미지 연달아 첨부하는 경우
-      if (state.contents[state.contents.length - 1].content.length === 0) {
-        state.contents.splice(state.contents.length - 1, 1);
-      }
       //선택한 파일 읽기
       const reader = new FileReader();
-      reader.onload = (event) => {
-        setState({
-          ...state,
-          contents: [
-            ...state.contents,
-            { type: "image", content: event.target.result },
-            { type: "text", content: "" },
-          ],
-        });
-      };
-      reader.readAsDataURL(selectedFile);
+      //편지 맨 뒤에 이미지 삽입하는 경우
+      if(cursor.pos === null || (cursor.index === (state.contents.length-1) && state.contents[cursor.index].content.substring(cursor.pos).length === 0)){
+        reader.onload = (event) => {
+          newArray.push({ type: "image", content: event.target.result });
+          newArray.push({ type: "text", content: "" });
+          setState({
+            ...state,
+            contents: newArray,
+          });
+        };
+        reader.readAsDataURL(selectedFile);
+        // 이미지 연달아 첨부하는 경우 또는 맨 위에 이미지 첨부하는 경우
+        if (state.contents[state.contents.length - 1].content.length === 0) {
+          newArray.splice(newArray.length - 1, 1);
+        }
+      }else{
+        const textBeforeClick = state.contents[cursor.index].content.substring(0, cursor.pos);
+        const textAfterClick = state.contents[cursor.index].content.substring(cursor.pos);
+        reader.onload = (event) => {
+          if(textBeforeClick.length !== 0 && textAfterClick.length !== 0){ //텍스트 사이에 이미지 삽입의 경우
+            newArray.splice(cursor.index, 1, {type: "text", content: textBeforeClick});
+            newArray.splice(cursor.index+1, 0, {type: "text", content: textAfterClick});
+            newArray.splice(cursor.index+1, 0, { type: "image", content: event.target.result });
+          }else if(textBeforeClick.length === 0){ //텍스트 맨 앞에 이미지 삽입의 경우
+            if(cursor.index === 0){ //편지 최상단
+              newArray.unshift({ type: "image", content: event.target.result });
+            } else{
+              newArray.splice(cursor.index-1, 0, { type: "image", content: event.target.result });
+            }
+          }else{ //텍스트 맨 뒤에 이미지 삽입의 경우
+            newArray.splice(cursor.index+1, 0, { type: "image", content: event.target.result });
+          }
+          setState({
+            ...state,
+            contents: newArray,
+          });
+          resizeTextarea(state.contents);
+        }
+        reader.readAsDataURL(selectedFile);
+      }
+      setCursor({pos: null, index: null});
+    }
+  };
+ 
+  //이미지 삭제
+  const deleteImage = (index) => {
+    if (window.confirm("이미지를 삭제할까요?")) {
+      const newItems = state.contents;
+      //앞뒤 content가 둘 다 text인 경우 병합
+      if(index !== 0 && newItems[index-1].type === "text" && newItems[index+1].type === "text"){
+        newItems[index - 1].content += newItems[index + 1].content;
+        newItems.splice(index, 2);
+      }
+      else{
+        newItems.splice(index, 1);
+      }
+      setState({
+        ...state,
+        contents: newItems,
+      });
+      resizeTextarea(state.contents);
     }
   };
 
+  //텍스트 1000자 제한
   const handleContentChange = (index, content) => {
     const newItems = [...state.contents];
     newItems[index].content = content;
@@ -138,23 +200,6 @@ const Write = () => {
       });
     } else {
       alert("텍스트 내용은 1000자를 초과할 수 없습니다.");
-    }
-  };
-  const deleteImage = async (index) => {
-    if (window.confirm("이미지를 삭제할까요?")) {
-      const newItems = state.contents;
-      if(index>0){
-        newItems[index - 1].content += "\n" + newItems[index + 1].content;
-        newItems.splice(index, 2);
-      }
-      else{
-        newItems.splice(index, 1);
-      }
-      await setState({
-        ...state,
-        contents: newItems,
-      });
-      await autoResizeTextarea(index);
     }
   };
 
@@ -177,13 +222,9 @@ const Write = () => {
       });
     }
   };
+
   return (
     <div className={["Write", receivedState.theme].join(" theme")}>
-      <div className="write_top">
-        <button className="btn_save" onClick={handleSave}>
-          <span>임시저장</span>
-        </button>
-      </div>
       <div className="write_center">
         <div className="to">
           <p>To. {receivedState.dear_name}</p>
@@ -194,11 +235,12 @@ const Write = () => {
               {item.type === "text" ? (
                 <textarea
                   value={item.content}
+                  onClick={(e)=>{setCursor({pos: e.target.selectionStart, index: index})}}
                   onChange={(e) => handleContentChange(index, e.target.value)}
                   className={`${state.alignType} index${index}`}
                   placeholder="여기에 작성하세요."
-                  onKeyDown={autoResizeTextarea}
-                  onKeyUp={autoResizeTextarea}
+                  onKeyDown={(e) => autoResizeTextarea(e, index)}
+                  onKeyUp={(e) => autoResizeTextarea(e, index)}
                   id={`${index}`}
                 />
               ) : (
