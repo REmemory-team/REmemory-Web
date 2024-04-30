9bfb268de8e907007733c22e149695a800c0b1ea
추억과 감정을 선물하는 타임캡슐 서비스, 리메모리 <155160067+RE-memory@users.noreply.github.com>
2024-04-30 21:12:21 +0900

    광고배너 문구추가-1-7

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -114,7 +114,7 @@ const Login = () => {
         browsingtopics
       ></iframe>
       <div>
-        <p style ={{fontSize:'10px',color:'#adb5db'}}>쿠팡 파트너스 활동을 통해 일정액의 수수료를 제공받을 수 있음</p>
+        <p style ={{fontSize:'10px',color:'#495057'}}>쿠팡 파트너스 활동을 통해 일정액의 수수료를 제공받을 수 있음</p>
       </div>
     </div>
   );
